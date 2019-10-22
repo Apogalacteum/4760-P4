32171f86503b9176162047819783b70fa428aab5
Alexander Hughey <hughey@hoare7.cs.umsl.edu>
2019-10-22 02:05:15 -0500

    3: clock and stuff

--- Project4Main.c
+++ Project4Main.c
@@ -7,8 +7,10 @@
 
 int main()
 {
-
-
-
+	
+	shmid = shmget(key, sizeof(int*), 0666 | IPC_CREAT);
+	int *clockSec = (int *)shmat(shmid, (void*)0,0);
+	int *clockNan = (int *)shmat(shmid, (void*)0,0);
+	
 	return 0;
 }
---
+++ Project4UserP.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+
+int main()
+{
+
+
+
+	return 0;
+}
